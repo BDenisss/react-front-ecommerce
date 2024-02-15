4b984996f58b2e263dc7ee5926f3ae4e7fc01018
BDenisss <bucspun.d@gmail.com>
2024-02-15 10:05:05 +0100

    :fire: delete cartSlice because we have now CartContext, so we get improvement in store.js :art:

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productApi } from '../services/productApi';
-import cartReducer from './CartSlice';
 
 export const store = configureStore({
     reducer: {
-        cart: cartReducer,
         [productApi.reducerPath]: productApi.reducer,
     },
-    // Ajouter le middleware d'API
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(productApi.middleware),
 });
